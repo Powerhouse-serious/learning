58ec9e277aaa139a36b6a6d501ee2d7fd65471f5
Powerhouse-serious <randomday20201@gmail.com>
2025-09-01 20:47:09 +0000

    Add home.js with cyan background and centered black "TURN BACK!" button

---
+++ home.js
@@ -0,0 +1,18 @@
+document.body.style.backgroundColor = "red";
+
+// Create button element
+const button = document.createElement("button");
+button.textContent = "TURN BACK!";
+button.style.position = "absolute";
+button.style.top = "50%";
+button.style.left = "50%";
+button.style.transform = "translate(-50%, -50%)";
+button.style.backgroundColor = "black";
+button.style.color = "white";
+button.style.fontSize = "2rem";
+button.style.padding = "1rem 2rem";
+button.style.border = "none";
+button.style.borderRadius = "8px";
+button.style.cursor = "pointer";
+
+// Add button to the body
